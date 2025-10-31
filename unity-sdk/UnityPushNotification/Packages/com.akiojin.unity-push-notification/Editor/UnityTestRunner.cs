01b23aeca0f67cca4a957926db3ce14525519c05
akiojin <akio-jinsenji@cloud-creative-studios.com>
2025-10-31 05:23:17 +0000

    fix(unity-sdk): fully qualify ScriptableObject usage

--- unity-sdk/UnityPushNotification/Packages/com.akiojin.unity-push-notification/Editor/UnityTestRunner.cs
+++ unity-sdk/UnityPushNotification/Packages/com.akiojin.unity-push-notification/Editor/UnityTestRunner.cs
@@ -9,7 +9,7 @@ namespace PushNotification.SDK.Editor
         [MenuItem("PushNotificationSDK/Run EditMode Tests")]
         public static void RunEditModeTests()
         {
-            var api = ScriptableObject.CreateInstance<TestRunnerApi>();
+            var api = UnityEngine.ScriptableObject.CreateInstance<TestRunnerApi>();
             var settings = new ExecutionSettings
             {
                 filters = new[]
