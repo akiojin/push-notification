65fb4404b21e3215d1d89b55d225192b2d08b78a
akiojin <akio-jinsenji@cloud-creative-studios.com>
2025-10-31 06:39:26 +0000

    fix(ios): ObjC ブリッジをUIKit環境限定に

--- ios-sdk/Sources/PushNotificationSDK/Bridging/PushNotificationSDKObjC.swift
+++ ios-sdk/Sources/PushNotificationSDK/Bridging/PushNotificationSDKObjC.swift
@@ -1,3 +1,4 @@
+#if canImport(UIKit)
 import Foundation
 import UserNotifications
 
@@ -35,3 +36,4 @@ public final class PushNotificationSDKObjC: NSObject {
         sdk.consumePendingNotificationResponse()
     }
 }
+#endif
